6c08f741b6680d93ac36e03175297cf9ba5c4afc
Pablo González L <pablgonz@yahoo.com>
2019-08-03 03:27:39 -0400

    new [full]

--- mybuild.lua
+++ mybuild.lua
@@ -1,6 +1,6 @@
 #!/usr/bin/env texlua
 -- Add ALL pkgs from list... (only need in firt run) , comment after
---os.execute("tlmgr install $(cat texlive/texlive_packages | grep -v -e '^[[:space:]]*$' -e '^#')")
+os.execute("tlmgr install $(cat texlive/texlive_packages | grep -v -e '^[[:space:]]*$' -e '^#')")
 -- Update TeXLive distribution
 os.execute("tlmgr update --self --all --no-auto-install")
 -- Compiling files using latexmk -cd /path/to/file.tex
