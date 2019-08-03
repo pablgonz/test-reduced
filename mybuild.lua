dd81ee65a4acd41da722b2db97903f212af94ed6
Pablo González L <pablgonz@yahoo.com>
2019-08-03 10:29:51 -0400

    Update [full]

--- mybuild.lua
+++ mybuild.lua
@@ -5,6 +5,6 @@ os.execute("tlmgr install $(cat texlive/texlive_packages | grep -v -e '^[[:space
 os.execute("tlmgr update --self --all --no-auto-install")
 -- Compiling files using latexmk -cd /path/to/file.tex
 os.execute("latexmk -e -pdf -cd src/main.tex")
-os.execute("latexmk -pdf -cd src/biber-mwe.tex")
-os.execute("latexmk -e '$pdflatex=q/pdflatex %O -shell-escape %S/' -pdf -cd src/minted.tex")
-os.execute("latexmk -xelatex -cd src/fonts.tex")
+--os.execute("latexmk -pdf -cd src/biber-mwe.tex")
+--os.execute("latexmk -e '$pdflatex=q/pdflatex %O -shell-escape %S/' -pdf -cd src/minted.tex")
+--os.execute("latexmk -xelatex -cd src/fonts.tex")
