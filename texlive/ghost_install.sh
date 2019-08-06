5d28200e12fdb811d264c7f90134788f6584d8c0
Pablo González L <pablgonz@yahoo.com>
2019-08-06 18:33:12 -0400

    A newwwwwmmmmm

--- texlive/ghost_install.sh
+++ texlive/ghost_install.sh
@@ -2,7 +2,8 @@
 if ! command -v gs > /dev/null; then
   wget https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs927/ghostscript-9.27-linux-x86_64.tgz
   tar -xzf ghostscript-9.27-linux-x86_64.tgz
-  cd $TRAVIS_BUILD_DIR/ghostscript-9.27-li
+  pwd
+  cd $HOME/ghostscript-9.27-li
   ./configure --prefix=~/gs
   make 
   make install
